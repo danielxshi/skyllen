99de96284d20d0a669acd4f59b68dfe84a60f205
Daniel Shi <danielshi@Daniels-MacBook-Pro.local>
2023-12-01 12:38:42 -0800

    Updating socials and parallax

--- app/components/SocialGallery.tsx
+++ app/components/SocialGallery.tsx
@@ -3,46 +3,49 @@ import Headline from "./headline";
 import { motion, AnimatePresence } from "framer-motion";
 import ContentfulImage from "@/lib/contentful-image";
 import Link from "next/link";
-import image1 from "../images/living-space-landing.webp";
-import image2 from "../images/pendrell-aerial.webp";
-import image3 from "../images/618-lobby.webp";
+import image1 from "@/public/images/ig/ig1.webp";
+import image2 from "@/public/images/ig/ig2.webp";
+import image3 from "@/public/images/ig/ig3.webp";
+import image4 from "@/public/images/ig/ig4.webp";
+import image5 from "@/public/images/ig/ig5.webp";
+import image6 from "@/public/images/ig/ig6.webp";
 
 const content = [
   {
     image: image1,
     key: 1,
-    url: "/",
-    alt: "",
+    url: "https://www.instagram.com/p/CxbE7RvvKAA/?img_index=1",
+    alt: "Skyllen Instagram Picture",
   },
   {
     image: image2,
     key: 2,
-    url: "/",
-    alt: "",
+    url: "https://www.instagram.com/p/CwWI5OtvqhA/",
+    alt: "Skyllen Instagram Picture",
   },
   {
     image: image3,
     key: 3,
-    url: "/",
-    alt: "",
+    url: "https://www.instagram.com/p/CvYXpq_vCli/",
+    alt: "Skyllen Instagram Picture",
   },
   {
-    image: image3,
-    key: 3,
-    url: "/",
-    alt: "",
+    image: image4,
+    key: 4,
+    url: "https://www.instagram.com/p/CunI6jeyI7o/?img_index=1",
+    alt: "Skyllen Instagram Picture",
   },
   {
-    image: image3,
-    key: 3,
-    url: "/",
-    alt: "",
+    image: image5,
+    key: 5,
+    url: "https://www.instagram.com/p/Ct-Kq0Uv3Pu/",
+    alt: "Skyllen Instagram Picture",
   },
   {
-    image: image3,
-    key: 3,
-    url: "/",
-    alt: "",
+    image: image6,
+    key: 6,
+    url: "https://www.instagram.com/p/CrhBVDGvtX3/",
+    alt: "Skyllen Instagram Picture",
   },
 ];
 
@@ -65,7 +68,7 @@ export default function first(props) {
 
       <div className="social-media-container overflow-x-scroll">
         {content.map((el) => (
-          <Link href="/">
+          <Link href={el.url}>
             <motion.div
               whileHover={{
                 scale: 1.05,
@@ -74,14 +77,25 @@ export default function first(props) {
               className="social-media-content relative z-10 block"
               key={el.key}
             >
-              <ContentfulImage src={el.image} fill={true} />
-          </motion.div>
+              <ContentfulImage
+                src={el.image}
+                fill={true}
+                quality={50}
+                alt={el.alt}
+              />
+            </motion.div>
           </Link>
         ))}
       </div>
 
       <div className="mt-8 underscore-cta ">
-        <Link href="/">Follow our social media</Link>
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.instagram.com/skyllenpacific/"
+        >
+          Follow our social media
+        </Link>
       </div>
     </>
   );
