be6459da8097c9571ebe4b7dc88cbcef3178255c
Daniel Shi <danielshi@Daniels-MBP.lan>
2023-12-06 18:34:08 -0800

    document undefined

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,7 +3,11 @@ import en, { en_project0, en_project1, en_project2 } from "@/src/i18n/en";
 import ch, { ch_project0, ch_project1, ch_project2 } from "@/src/i18n/ch";
 
 export function getLocale() {
-  if (typeof window !== 'undefined' && window.localStorage) {
+  if (
+    typeof window !== "undefined" &&
+    typeof document !== "undefined" &&
+    window.localStorage
+  ) {
     return window.localStorage.getItem("locale") ?? "en";
   } else {
     return "en";
@@ -31,10 +35,8 @@ export const messages = { en, ch };
 
 // write a 2d array with 3 element, each element is a {en, ch} object
 export const projects = {
-  "en": 
-  {0: en_project0, 1: en_project1, 2: en_project2},
-  "ch": 
-  {0: ch_project0, 1: ch_project1, 2: ch_project2},
+  en: { 0: en_project0, 1: en_project1, 2: en_project2 },
+  ch: { 0: ch_project0, 1: ch_project1, 2: ch_project2 },
 };
 
 // export const projects2 = [
